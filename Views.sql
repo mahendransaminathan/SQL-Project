34afa158382771e73c46f3fba929818dbdf88349
mahendransaminathan <125678754+mahendransaminathan@users.noreply.github.com>
2025-04-06 11:38:45 +0100

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,4 +1,9 @@
+DROP VIEW IF EXISTS ActiveCustomers;
+Go
 CREATE VIEW ActiveCustomers AS
 SELECT customer_id, name, address
 FROM Customers
 WHERE name like 'Bob%';
+Go
+
+Select * from ActiveCustomers;
